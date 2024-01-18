571451ea88d6b1deac56211cd37dad30c94369d6
Tom <tomagain@mail.com>
2024-01-18 19:34:55 +0100

    Reworked login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { UserAddComponent } from './user-add/user-add.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ConstructionSiteComponent } from './construction-site/construction-site.component';
 import { ConstructionSiteDetailsComponent } from './construction-site-details/construction-site-details.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { ConstructionSiteAddComponent } from './construction-site-add/construction-site-add.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
@@ -46,7 +52,10 @@ import { ConstructionSiteAddComponent } from './construction-site-add/constructi
     HttpClientModule,
     MatDialogModule,
     MatSnackBarModule,
-    MatCardModule
+    MatCardModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
